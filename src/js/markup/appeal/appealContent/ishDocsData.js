5fcaa400ee624d17ed7950a9b3e5354c3dfa2c69
Boris Kotyrev <kotyrev@stdpr.ru>
2019-03-04 16:05:00 +0300

    "Обращения -> Новое входящее": экран "Исходящие документы"

--- src/js/markup/appeal/appealContent/ishDocsData.js
+++ src/js/markup/appeal/appealContent/ishDocsData.js
@@ -1,45 +1,61 @@
 import React from 'react'
-import { FieldArray, reduxForm } from 'redux-form/immutable'
-import {EInput,FInput} from '../../components/finput.js'
-import {EAutocomplete,FAutocomplete} from '../../components/fautocomplete.js'
-import {ECheckbox,FCheckbox} from '../../components/checkbox.js'
-import {ESwitch,FSwitch} from '../../components/switch.js'
-import {ESelect,FSelect} from  '../../components/select.js'
-import {EPicker,FPicker} from '../../components/picker.js'
-import {EIshDocList,FIshDocList} from '../subForms/ishDocList.js'
+import {FieldArray, reduxForm} from 'redux-form/immutable'
+import {EInput, FInput} from '../../components/finput.js'
+import {EAutocomplete, FAutocomplete} from '../../components/fautocomplete.js'
+import {ECheckbox, FCheckbox} from '../../components/checkbox.js'
+import {ESwitch, FSwitch} from '../../components/switch.js'
+import {ESelect, FSelect} from '../../components/select.js'
+import {EPicker, FPicker} from '../../components/picker.js'
+import {EIshDocList, FIshDocList} from '../subForms/ishDocList.js'
 import mapping from './mapping.js'
+import {Button, Card, Layout, Tag} from 'element-react'
+import {FTopicList} from "../subForms/topicList";
 
-const header = 'Исходящие документы';
+const headerTitle = 'Исходящие документы';
 
 const IshDocsData = props => {
-    const { handleSubmit, pristine, nextPage, prevPage, submitting, disabled } = props
-    
+    const {handleSubmit, pristine, nextPage, prevPage, submitting, disabled} = props;
+
     return (
-       <div className='appealSection'>
-          <h2>{header}</h2>
-          <div className='appealContent'>
-          <form onSubmit={handleSubmit}>
+        <div className='px18 py18'>
+            <Layout.Row gutter="20">
+                <Layout.Col span="16" offset="4">
+                    <Card className="box-card" header={
+                        <div className='flex-parent flex-parent--center-cross flex-parent--space-between-main py12'>
+                            <h3 className='ap-h3 flex-parent flex-parent--center-cross'>
+                                {headerTitle}
+                            </h3>
 
-            <FieldArray name='ishDocsData' component={FIshDocList} disabled={disabled} />    
+                            {disabled
+                                ? null
+                                : (<div>
+                                    <Tag type="gray" className='mx12'>6/8</Tag>
 
-            <div>
-              {disabled 
-              ? null 
-              : (<div>
-                  <button type="button" onClick={prevPage}>Previous</button>
-                  <button type="submit" onClick={nextPage}>Submit</button>
-                </div>)
-            }
-            </div>
-          </form>
+                                    <Button.Group>
+                                        <Button type="primary" size='small' icon="arrow-left"
+                                                onClick={prevPage}/>
+                                        <Button type="primary" size='small' onClick={nextPage}>
+                                            <i className="el-icon-arrow-right el-icon-right"/>
+                                        </Button>
+                                    </Button.Group>
+                                </div>)
+                            }
+                        </div>
+                    }>
+                        <form onSubmit={handleSubmit}>
+                            <h4 className='ap-h4'>Список исходящих документов</h4>
+                            <FieldArray name='ishDocsData' component={FIshDocList} disabled={disabled}/>
+                        </form>
+                    </Card>
+                </Layout.Col>
+            </Layout.Row>
         </div>
-      </div>
     )
 } //
 
 export default reduxForm({
-  form: 'appeal', // <------ same form name                       disabled={pristine || submitting}
-  destroyOnUnmount: false, // <------ preserve form data
-  forceUnregisterOnUnmount: true//, // <------ unregister fields on unmount
-  //validate
+    form: 'appeal', // <------ same form name                       disabled={pristine || submitting}
+    destroyOnUnmount: false, // <------ preserve form data
+    forceUnregisterOnUnmount: true//, // <------ unregister fields on unmount
+    //validate
 })(IshDocsData)
