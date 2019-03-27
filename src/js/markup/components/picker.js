e378872160fe6a5b1fe248463f58e79e27347f67
Boris Kotyrev <boris.kotyrev@gmail.com>
2019-03-27 14:30:47 +0300

    #23335: слово "Сегодня" заменено на число и выделено цветом

--- src/js/markup/components/picker.js
+++ src/js/markup/components/picker.js
@@ -1,53 +1,61 @@
 import React from 'react'
 import withValidators from './tooltipper.js'
-import {TimePicker,DatePicker} from 'element-react'
+import {TimePicker, DatePicker} from 'element-react'
 import MaskedInput from 'react-text-mask'
 
 // Element component
-const ofs =  new Date().getTimezoneOffset()*60*1000;
+const ofs = new Date().getTimezoneOffset() * 60 * 1000;
 
 class APicker extends React.Component {
-	render(){ 
-		const {timepicker,datepicker,datetimepicker,onChange,reduxformfield} = this.props;
-		let value = reduxformfield ? this.props.input.value : this.props.value;
+    render() {
+        const {timepicker, datepicker, datetimepicker, onChange, reduxformfield} = this.props;
+        let value = reduxformfield ? this.props.input.value : this.props.value;
         value = value || null;
 
-        if (typeof value=='string'){
-        	value = new Date(Date.parse(value) + ofs)
-			//value = new Date(Date.parse('2019-03-13T00:00:00.000Z') + ofs)
+        if (typeof value == 'string') {
+            value = new Date(Date.parse(value) + ofs)
+            //value = new Date(Date.parse('2019-03-13T00:00:00.000Z') + ofs)
         }
 
-		const customChanger = (newVal)=>{
-			let changer = null;
-			if (reduxformfield){
-				this.props.input.onChange(newVal);
-			} else if (changer=this.props.onChange){ 
-				changer(newVal);
-			} else if (changer=this.props.onValidate){ ;
-				changer(newVal);
-			}
-		}
+        const customChanger = (newVal) => {
+            let changer = null;
+            if (reduxformfield) {
+                this.props.input.onChange(newVal);
+            } else if (changer = this.props.onChange) {
+                changer(newVal);
+            } else if (changer = this.props.onValidate) {
+                ;
+                changer(newVal);
+            }
+        }
 
-		const p = Object.assign({},this.props);
-		delete p.onChange;
+        const datePickerFormatToday = () => {
+            requestAnimationFrame(() => {
+                document.querySelector('.available.today').textContent = new Date().getDate();
+            })
+        };
 
-       	if (timepicker){ 
-			return <TimePicker {...p} value={value} onChange={customChanger}  />;
-		} else { //
-			const isShowTime=!!datetimepicker;
-	       	return <DatePicker {...p} isShowTime={isShowTime} value={value} onChange={customChanger} />
-		} //	
-	} //
+        const p = Object.assign({}, this.props);
+        delete p.onChange;
+
+        if (timepicker) {
+            return <TimePicker {...p} value={value} onChange={customChanger}/>;
+        } else { //
+            const isShowTime = !!datetimepicker;
+            return <DatePicker {...p} isShowTime={isShowTime} value={value} onFocus={datePickerFormatToday}
+                               onChange={customChanger}/>
+        } //
+    } //
 }
 
-const EPicker = withValidators(APicker,{forceValidate: true});
+const EPicker = withValidators(APicker, {forceValidate: true});
 
 // redux Forrm component
 const FPicker = (props) => {
-	const { input, label, type, meta: { touched, error } } = props;
-	const newProps = Object.assign({},props);
-	//delete newProps.meta;
-	return <EPicker {...newProps} reduxformfield="true" />
+    const {input, label, type, meta: {touched, error}} = props;
+    const newProps = Object.assign({}, props);
+    //delete newProps.meta;
+    return <EPicker {...newProps} reduxformfield="true"/>
 } //
 
-export {EPicker,FPicker};
+export {EPicker, FPicker};
