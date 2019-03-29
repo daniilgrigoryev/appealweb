fb150b9d535139110580dc4889e48e83b119f39c
Shatalova <shatalova@stdpr.ru>
2019-03-29 10:28:33 +0300

    Autocomplete fixed, letters data prepared for db

--- src/js/markup/letter/inc/incomingLetter.js
+++ src/js/markup/letter/inc/incomingLetter.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as _ from 'lodash'
+import {compose} from 'redux'
 import {connect} from 'react-redux'
 import {Button, Input, Card, Layout} from 'element-react'
 
@@ -11,7 +12,34 @@ import IncLetterIspoln from './sub/incLetterIspoln.js'
 import IncLinkInner from './sub/incLinksInner.js'
 import IncLinkScan from './sub/incLinksScan.js'
 
-export default class IncomingLetter extends React.Component {
+import {reduxForm} from 'redux-form/immutable'
+
+import {post} from '../../../services/ajax.js'
+
+const alias = "INC_LETTER_PUSH"
+
+class IncomingLetter extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.register = this.register.bind(this)
+    }
+
+    register(){
+        const a = this;
+
+        const {formData,dispatch,change} = a.props;
+        const data = JSON.stringify(Object.assign({},formData.values));
+        const jsonMode = true;
+
+        post('db/push', {alias,data,jsonMode}).then(x => {
+        }).catch(x=>{
+            dispatch(messageSet(x,'error'));
+            console.error(x);
+            a.forceUpdate();
+        });  
+
+    }
 
     render() {
         return (
@@ -30,8 +58,26 @@ export default class IncomingLetter extends React.Component {
                         <IncLinkInner/>
                         <IncLinkScan/>
                     </Card>
+                    <div className="ap-footer">
+                    <Button className='mr24' onClick={this.register}>Зарегистрировать</Button>
+                    </div>
                 </Layout.Col>
             </Layout.Row>
-        );
+        );//
     }
 }
+
+const mapStateToProps = (state) => {
+    let formData = state.getIn(['form','letter_incoming']);
+    formData && (formData = formData.toJS());
+    return {formData};
+}
+
+export default compose(
+    connect(mapStateToProps),
+    reduxForm({
+        form: 'letter_incoming', // <------ same form name
+        destroyOnUnmount: false, // <------ preserve form data
+        forceUnregisterOnUnmount: true, // <------ unregister fields on unmount
+    })
+)(IncomingLetter)
--- src/js/markup/letter/out/mapping.js
+++ src/js/markup/letter/out/mapping.js
@@ -13,14 +13,14 @@ export default {
 		VID_DOC: 		 {name: 'vid_doc',		  		label: 'Вид документа',	            key: 'doc_vid'},
 		SUMMARY: 		 {name: 'summary',		  		label: 'Краткое содержание'},
 		NOTES: 		     {name: 'notes',		  		label: 'Примечание'},
-		DELIV_TYPE: 	 {name: 'delivery_type',	  	label: 'Вид отправки',				key: 'delivery_type'},
+		DELIV_TYPE: 	 {name: 'delivery_type',	  	label: 'Вид отправки',				key: 'delivery_method'},
 		SHEETS_COUNT: 	 {name: 'sheets_count', 	  	label: 'Кол-во листов'}
 	},
 	outLetterHead: {
 		ZAJAV_NDOC:     {name: 'zajav_ndoc',	 		label: '№ документа'},
 		ZAJAV_DATE:     {name: 'zajav_date',	  		label: 'Дата'},
-		SIGNER: 		{name: 'signer',	 			label: 'Подписал'},
-		ZAJAV_SIGNER: 	{name: 'zajav_signer',			label: 'Исполнитель'},
+		SIGNER: 		{name: 'signer',	 			label: 'Подписал',					key: 'DECISION_MAKER'},
+		ZAJAV_SIGNER: 	{name: 'zajav_signer',			label: 'Исполнитель',				key: 'EMPLOYESS_LIST'},
 		SER_VH_DOC: 	{name: 'inc_ser',				label: 'Серия входящего документа'},
 		NUM_VH_DOC: 	{name: 'inc_num',				label: 'Номер входящего документа'},
 		INC_DAT: 		{name: 'inc_dat',		  		label: 'Дата'},
